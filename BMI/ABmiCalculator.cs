1c7730454cc6f932c2a44f9b8e8286cb9d7c2cca
Kuei-PC\Kuei <ragnakuei@livemail.tw>
2017-03-28 15:15:37 +0800

    降 .net framework 版本 (測試)

--- BMI/ABmiCalculator.cs
+++ BMI/ABmiCalculator.cs
@@ -1,22 +1,22 @@
 ﻿namespace BMI
 {
-    public abstract class ABmiCalculator
+public abstract class ABmiCalculator
+{
+    protected double _upper;
+    protected double _lower;
+
+    public double Bmi { get; protected set; }
+    public string Result { get; protected set; }
+
+    protected void DoCalc()
     {
-        protected double _upper;
-        protected double _lower;
-
-        public double Bmi { get; protected set; }
-        public string Result { get; protected set; }
-
-        protected void DoCalc()
-        {
-            SetBmiBoundary();
-            CalcBmi();
-            SetBmiResult();
-        }
-
-        protected abstract void CalcBmi();
-        protected abstract void SetBmiBoundary();
-        protected abstract void SetBmiResult();
+        SetBmiBoundary();
+        CalcBmi();
+        SetBmiResult();
     }
+
+    protected abstract void CalcBmi();
+    protected abstract void SetBmiBoundary();
+    protected abstract void SetBmiResult();
+}
 }
